fa0a791022dddfd789a70aac7d166e6642f58242
kushal021 <kushalrajyaguru021@gmail.com>
2023-07-10 22:53:28 +0530

    Initial commit

---
+++ src/components/MapIndia.js
@@ -0,0 +1,140 @@
+import React, { useEffect, useRef, useState } from "react";
+import India from "@svg-maps/india";
+import { SVGMap } from "react-svg-map";
+import "react-svg-map/lib/index.css";
+import styled from "styled-components";
+import MapState from "./MapState";
+
+const MapIndia = () => {
+  const [hoveredState, setHoveredState] = useState("");
+  const [defaultSelectedState, setDefaultSelectedState] = useState([]);
+  const [state, setState] = useState();
+  const [tooltipPosition, setTooltipPosition] = useState({
+    display: "none",
+  });
+
+  const selectedStateColor = [
+    "#FF6666",
+    "#FF8989",
+    "#FCAEAE",
+    "#FFEADD",
+    "#FF9B9B",
+    "#FFD6A5",
+    "#FFFEC4",
+    "#CBFFA9",
+  ];
+
+  function onLocationClick(event) {
+    let stateArray = [...defaultSelectedState];
+    if (stateArray?.includes(event.target.getAttribute("name"))) {
+      stateArray = stateArray?.filter(
+        (ele) => ele !== event.target.getAttribute("name")
+      );
+    } else {
+      stateArray.push(event.target.getAttribute("name"));
+    }
+    setDefaultSelectedState([...stateArray]);
+    localStorage.setItem("mapData", JSON.stringify([...stateArray]));
+    const stateData = India?.locations?.find(
+      (location) => location.id === event.target.id
+    );
+
+    if (stateData) {
+      setState(stateData);
+    }
+  }
+
+  const clearAllFunction = () => {
+    setDefaultSelectedState([]);
+    localStorage.clear();
+  };
+
+  const handleStateHover = (event) => {
+    setHoveredState(event.target.attributes.name.value);
+    const tooltipStyle = {
+      display: "block",
+      top: event.clientY + 10,
+      left: event.clientX - 100,
+    };
+    setTooltipPosition({ ...tooltipStyle });
+  };
+
+  const handleLocationMouseOut = () => {
+    setHoveredState(null);
+    setTooltipPosition({ display: "none" });
+  };
+
+  useEffect(() => {
+    const mapData = JSON.parse(localStorage.getItem("mapData")) || ["Gujarat"];
+    setDefaultSelectedState([...mapData]);
+  }, []);
+
+  return (
+    <>
+      <div className="main-div">
+        <article className="examples__block">
+          <div className="title">
+            <div>
+              <h2 className="examples_block_title">India 🗺️</h2>
+              <StyledButton onClick={() => clearAllFunction()}>
+                Clear all
+              </StyledButton>
+            </div>
+          </div>
+          <div className="examples_blockmap examplesblock_map--india">
+            <StyledSVGMap
+              map={India}
+              onLocationClick={onLocationClick}
+              $selectedLocations={defaultSelectedState}
+              onStateLeave={() => setHoveredState(null)}
+              onLocationMouseMove={handleStateHover}
+              onLocationMouseOut={handleLocationMouseOut}
+              $selectedStateColor={selectedStateColor}
+              $defaultSelectedState={defaultSelectedState}
+            />
+            <div className="examples_blockmap_tooltip" style={tooltipPosition}>
+              {hoveredState}
+            </div>
+          </div>
+        </article>
+        <div className="mapView">
+          <p>{state?.name}</p>
+          {/* <p>{state?.id}</p> */}
+          {state && <MapState data={state?.path} name={state?.name} />}
+        </div>
+      </div>
+    </>
+  );
+};
+
+const StyledSVGMap = styled(SVGMap)`
+  ${({ $defaultSelectedState, $selectedStateColor }) =>
+    $defaultSelectedState?.map(
+      (state, index) => `
+  & [aria-label="${state}"] {
+    fill: ${$selectedStateColor[index < 9 ? index : 0]};
+  }
+
+  &[aria-label="${state}"]:hover {
+    fill: #fff; /* Set the color when hovering */
+  }
+`
+    )}
+
+  width: 70%;
+  height: auto;
+  stroke: #666;
+  stroke-width: 1;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+`;
+
+const StyledButton = styled.button`
+  background-color: #e4f0ec;
+  padding: 0px 34px;
+  border-radius: 17px;
+  display: inline-block;
+  height: 40px;
+  cursor: pointer;
+`;
+export default MapIndia;
---
+++ src/components/MapState.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const MapState = ({ data, name }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      // viewBox="0 0 3000 3000"
+      viewBox="0 0 612 696"
+      className="sc-hKwBBi ezlnGP"
+      role="none"
+      aria-label={`Map of ${name}`}
+      // width="50%"
+      // height="50%"
+    >
+      <path
+        id="wb"
+        name={name}
+        d={data}
+        className="svg-map__location"
+        tabIndex="0"
+        role="none"
+        aria-label={name}
+      ></path>
+    </svg>
+  );
+};
+
+export default MapState;
